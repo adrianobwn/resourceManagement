f2228f0f9f55f4873aa6f939ededcafb9be9dbb8
adrianobawan <167875657+adrianobwn@users.noreply.github.com>
2026-01-20 15:24:22 +0700

    Test

--- backend/src/main/java/com/resourceManagement/resourceManagement/controller/helloController.java
+++ backend/src/main/java/com/resourceManagement/resourceManagement/controller/helloController.java
@@ -9,7 +9,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:5174")
+@CrossOrigin(origins = "http://localhost:5173")
 public class helloController {
 
     @GetMapping("/hello")
